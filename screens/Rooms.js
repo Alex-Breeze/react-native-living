7ca9b823e570f4a76414141e7f1be96eecc83a3a
harebu <bugu1986@126.com>
2016-10-17 11:13:54 +0800

    use exponent nav

--- screens/Rooms.js
+++ screens/Rooms.js
@@ -11,38 +11,19 @@ import {
     Alert,
     Platform
 } from 'react-native';
-import {Navigation} from 'react-native-navigation';
 import NoRoom from './rooms/NoRoom'
 import Rooms from './rooms/Rooms'
 
 export default class FirstTabScreen extends Component {
-  static navigatorButtons = {
-    leftButtons: [{
-      icon: require('../img/icon-menu.png'),
-      //title: 'Menu',
-      id: 'menu'
-    }]
-  };
-  static navigatorStyle = {
-    navBarBackgroundColor: 'rgba(0, 0, 0, 0.9)',
-    navBarButtonColor: '#ffffff',
-    drawUnderNavBar: true,
-    //navBarTranslucent: true,
-    //navBarBlur: true,
-  };
+  static route = {
+    navigationBar: {
+      title: 'About',
+      tintColor: "#000",
+    },
+  }
 
   constructor(props) {
     super(props);
-    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
-  }
-
-  onNavigatorEvent(event) {
-    if (event.id === 'menu') {
-      this.props.navigator.toggleDrawer({
-        side: 'left',
-        animated: true
-      });
-    }
   }
 
   render() {
@@ -51,20 +32,6 @@ export default class FirstTabScreen extends Component {
           <NoRoom />
         </View>
     );
-  }
-
-  onPushPress() {
-    this.props.navigator.push({
-      title: "More",
-      screen: "example.PushedScreen"
-    });
-  }
-
-  onPushStyledPress() {
-    this.props.navigator.push({
-      title: "Styled",
-      screen: "example.StyledScreen"
-    });
   }
 
 }
--- screens/index.js
+++ screens/index.js
@@ -2,14 +2,14 @@
  * Created by buhe on 2016/10/11.
  */
 
-import {Navigation} from 'react-native-navigation';
+import {
+    createRouter,
+    NavigationProvider,
+} from '@exponent/ex-navigation';
 
 import Rooms from './Rooms';
-import SideMenu from './SideMenu';
-
-
-// register all screens of the app (including internal ones)
-export function registerScreens() {
-  Navigation.registerComponent('Rooms', () => Rooms);
-  Navigation.registerComponent('SideMenu', () => SideMenu);
-}
+import AboutScreen from './AboutScreen';
+export default Router = createRouter(() => ({
+  home: () => Rooms,
+  about: () => AboutScreen,
+}));
